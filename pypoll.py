d0ac9148e4867587f180c0a900cd499f07fafb53
Jonathan Tree <46392976+jonathantree@users.noreply.github.com>
2021-12-30 11:45:47 -0800

    revised pypoll.py and created relic script

--- pypoll.py
+++ pypoll.py
@@ -5,46 +5,30 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on the popular vote
 
-#OLD FILE LOAD  SCRIPT
-#{}{}}{}{}{}{}{}{{}{}{}{}{}}{{{{{{{{{{{{{}}}}}}}}}}}}}
-#path to data: .\Resources\election_results.csv
-# Assign a variable for the file to load and the path.
-#file_to_load = '.\Resources\election_results.csv'
-
-# Open the election results and read the file.
-#election_data = open(file_to_load, 'r')
-
-# To do: perform analysis.
-
-# Close the file.
-#election_data.close()
-
-# Open the election results and read the file
-#with open(file_to_load) as election_data:
-
-     # To do: perform analysis.
-#     print(election_data)
 
 #=======================================================
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # Print the file object.
-     print(election_data)
+     # Read the file object with the reader function.
+     file_reader = csv.reader(election_data)
+     
+     # Print the header row.
+     headers = next(file_reader)
+     print(headers)
 
-#==========================================================
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("Analysis", "election_analysis.txt")
-
-# Using the open() function with the "w" mode we will write data to the file.
-# !!Not needed when using the with statement
-# outfile = open(file_to_save, "w")
+     # Print each row in the CSV file.
+     #for row in file_reader:
+     #     print(row)
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
@@ -53,9 +37,3 @@ with open(file_to_save, "w") as txt_file:
      txt_file.write("Counties in the Election")
      txt_file.write("\n-------------------------")
      txt_file.write("\nArapahoe\nDenver\nJefferson")
-
-# Write some data to the file.
-#outfile.write("Hello World")
-
-# Close the file (not needed if using a with statement)
-#outfile.close()
